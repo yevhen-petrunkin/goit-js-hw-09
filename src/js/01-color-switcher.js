7bed5fb2142c5a7de800a617a8fe55aaba5507a9
yevhen-petrunkin <novamovaxxi@gmail.com>
2022-11-19 17:36:23 +0200

    Commit color-switcher Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const colorSwitcher = {
+  colorSwitchId: null,
+  bodyRef: document.querySelector('body'),
+  startBtnRef: document.querySelector('button[data-start]'),
+  stopBtnRef: document.querySelector('button[data-stop]'),
+};
 
+colorSwitcher.startBtnRef.addEventListener('click', onStartBtnClick);
+colorSwitcher.stopBtnRef.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  colorSwitcher.startBtnRef.setAttribute('disabled', '');
+  colorSwitcher.colorSwitchId = setInterval(
+    () => (colorSwitcher.bodyRef.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+function onStopBtnClick() {
+  colorSwitcher.startBtnRef.removeAttribute('disabled');
+  clearInterval(colorSwitcher.colorSwitchId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
